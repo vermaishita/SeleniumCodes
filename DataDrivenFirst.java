fd032b0fc9ad22a41d51fb415a62c532dfac3de7
vermaishita <35197486+vermaishita@users.noreply.github.com>
2019-12-10 12:40:08 +0530

    Add files via upload

--- DataDrivenFirst.java
+++ DataDrivenFirst.java
@@ -1,38 +1,51 @@
 package DataDriven;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class DataDrivenFirst {
-	String data;
+	WebDriver driver;
+	String data,Username,Password,path;
+	XSSFRow row;
+	XSSFCell cell;
+	@Test
 	public void getData() throws IOException 
 	{
+		path=System.getProperty("user.dir")+"//Driver//chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
 		FileInputStream fs= new FileInputStream("E:\\Selenium Files\\LoginData.xlsx");
-		//Access Workbook
 		XSSFWorkbook wb=new XSSFWorkbook(fs);
-		
-		//Access sheet from workbook
-		XSSFSheet sheet=wb.getSheet("Login");
-		
-		//Access row from the work sheet
+		XSSFSheet sheet=wb.getSheet("Login");		
 		int count=sheet.getLastRowNum();
-		System.out.println("Total number of rows=" +count);
-		
-		XSSFRow row=sheet.getRow(0);
-		
-		//Fetch row from 0 row
-		XSSFCell cell=row.getCell(1);
-		data=cell.getStringCellValue();
-		System.out.println("data is:" +data);
+		System.out.println("Total number of rows=" +count);	
+		for(int i=0;i<=sheet.getLastRowNum();i++)
+		{
+			
+			row=sheet.getRow(i);			 
+			{
+				Username=row.getCell(0).toString();
+				Password=row.getCell(1).toString();
+			//	System.out.println("Username");
+				driver.findElement(By.id("txtUsername")).clear();
+				driver.findElement(By.id("txtPassword")).clear();
+				driver.findElement(By.id("txtUsername")).sendKeys(Username);
+				driver.findElement(By.id("txtPassword")).sendKeys(Password);
+				driver.findElement(By.cssSelector("#btnLogin")).submit();
+			//	driver.navigate().back();
+			}
+		}				
 	}
 	public static void main(String[] args) throws IOException {
 		DataDrivenFirst ddf=new DataDrivenFirst();
 		ddf.getData();
 	}
-
 }
